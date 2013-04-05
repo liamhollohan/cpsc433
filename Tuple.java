880f8b25bf6bc0ad5c61f186ff81172d080fbaf1
kingofthe187 <slove611@gmail.com>
2013-04-05 14:58:29 -0600

    Tuple updated. Need to update the comments still though.

--- Tuple.java
+++ Tuple.java
@@ -1,49 +1,41 @@
 package cpsc433;
 
-import java.util.ArrayList;
-
 //****************************************************************************************************************
-//Tuple is a class which holds a room number and a list of people that are assigned to a room.  It acts as a pair*
-//in which the first element is a room name represented as a sting and the second element is an arraylist of     *
-//strings which represent the people assigned to a room.  The arraylist will be at most two people long as       *
-//specified by the hard constraints.																			 *
+//Tuple is a class which holds a room number and name of a person that is assigned to a room.  It acts as a pair *
+//in which the first element is a room name represented as a sting and the second element is another string      *
+//representing the person assigned to the room.																	 *
 //****************************************************************************************************************
 public class Tuple {
 
-	private String room;
-	private ArrayList<String> people = new ArrayList();
+	private Room room;
+	private Person name;
 	
 	//Constructor for the class tuple.  It takes in a room and a person and assigns them as a pair.
-	public  Tuple(String room, String person)
+	public  Tuple(Room room, Person person)
 	{
 		setRoom(room);
-		setPeople(person);
+		setPerson(person);
 	}
 	
-	public void setRoom(String room)
+	public void setPerson(Person person) 
+	{
+		this.name = person;
+		
+	}
+	
+	public Person getPerson()
+	{
+		return this.name;
+	}
+
+	public void setRoom(Room room)
 	{
 		this.room = room;
 	}
 	
-	public String getRoom()
+	public Room getRoom()
 	{
 		return this.room;
 	}
-	
-	//****************************************************************************************************************
-	//Set people is a method which adds a person to a room.  If the person already exists in the room,  an error is  *
-	//output to the console.  Otherwise, the person is added to the rear of the arraylist.							 *
-	//****************************************************************************************************************
-	public void setPeople(String person)
-	{
-		if (!(this.people).contains(person))
-			(this.people).add(person);
-		else
-			System.out.println(" -> Error (SetPeople)");
-	}
-	
-	public ArrayList<String> getPeople()
-	{
-		return this.people;
-	}
 }
+	
